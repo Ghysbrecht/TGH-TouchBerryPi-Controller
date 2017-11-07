a29a2b791beeff7f7687b0277f5e8aa7ce9383f3
Thomas Ghysbrecht <thomas.ghysbrecht@student.vives.be>
2017-11-07 13:40:09 +0100

    Added led class

---
+++ lib/PiLed.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <iostream>
+#include "PiI2C.h"
+
+class PiLed{
+
+private:
+  int address;
+  PiI2C* i2c;
+
+public:
+  PiLed();
+  void setBrightness(float brightness);
+  void setChannel(int channel, float brightness);
+  void enable();
+  void initialize();
+  void setOutputState(int ledout0, int ledout1, int ledout2, int ledout3);
+  void setLed(int led, float red, float green, float blue);
+
+private:
+  void setRegister(int reg, int value);
+
+
+
+};
